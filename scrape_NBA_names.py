0994f7faf65ef402b419c7a97d79295bd788da75
Sarim Zia Khan <sarim.szk@gmail.com>
2018-05-22 05:51:10 -0400

    Update scrape_NBA_names.py

--- scrape_NBA_names.py
+++ scrape_NBA_names.py
@@ -2,6 +2,15 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
+class Player():
+	def __init__(self):
+		self.name = ""
+		self.link = ""
+
+
+
+player_list = []
+
 driver = webdriver.PhantomJS(executable_path = r'C:\Users\sarim\Desktop\Scraper\phantomjs.exe')
 driver.get('http://www.nba.com/players')
 pause = 0
@@ -16,9 +25,25 @@ while True:
 
 html_doc = driver.page_source
 soup = BeautifulSoup(html_doc, 'lxml')
-row_nba_player_index = soup.find('section', class_ = 'row nba-player-index__row')
-names = row_nba_player_index.find_all('p', class_ = 'nba-player-index__name')
-for i in names:
-	print i.text
+
+nba_players = soup.find('section', class_ = 'row nba-player-index__row')
+player_names_links = nba_players.find_all('a', class_ = None)
+
+# p = player_names_links[1]
+# print p['title']
+# print p['href']
+
+
+addLink = 'http://www.nba.com'
+for i in player_names_links:
+	newPlay = Player()
+	corrLink = addLink + i['href']
+	newPlay.link = corrLink
+	newPlay.name = i['title']
+	player_list.append(newPlay)
+
+for one_player in player_list:
+	print one_player.name
+	print one_player.link
 
 driver.quit()
