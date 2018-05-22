d9db5da84fd9bb69c2078328aaed0c748611873e
Sarim Zia Khan <sarim.szk@gmail.com>
2018-05-22 07:32:09 -0400

    scrapes position and height/weight
    Now collects player position, height and weight as well.

--- scrape_NBA_names.py
+++ scrape_NBA_names.py
@@ -6,6 +6,8 @@ class Player():
 	def __init__(self):
 		self.name = ""
 		self.link = ""
+		self.pos = ""
+		self.dim = ""
 
 player_list = []
 
@@ -26,22 +28,30 @@ soup = BeautifulSoup(html_doc, 'lxml')
 
 nba_players = soup.find('section', class_ = 'row nba-player-index__row')
 player_names_links = nba_players.find_all('a', class_ = None)
+player_height_weight = nba_players.find_all('div', class_ = 'nba-player-index__details')
 
 # p = player_names_links[1]
 # print p['title']
 # print p['href']
 
-
+j = 0
 addLink = 'http://www.nba.com'
 for i in player_names_links:
 	newPlay = Player()
+	indiv = player_height_weight[j]
+	span = indiv.find_all('span')
 	corrLink = addLink + i['href']
 	newPlay.link = corrLink
 	newPlay.name = i['title']
+	newPlay.pos = span[0].text
+	newPlay.dim = span[1].text
 	player_list.append(newPlay)
+	j = j+1
 
 for one_player in player_list:
 	print one_player.name
+	print one_player.pos
+	print one_player.dim
 	print one_player.link
 
 driver.quit()
